45c0af03b846ef9fe3f346e62292b80e91779c76
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-02-04 22:40:35 +0100

    feat(modules): add initial mysql module for azure

--- app/modules/azure/mysql/main.tf
+++ app/modules/azure/mysql/main.tf
@@ -1,1 +1,62 @@
 # This is where you put your resource declaration
+locals {
+  tags = merge(var.tags, {
+    managed_by     = "OpenTofu"
+    module_name    = "azure/mysql"
+    module_version = "v0.0.1"
+    environment    = var.environment
+  }) 
+}
+
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_mysql_flexible_server" "this" {
+  name                = coalesce(var.name, "mysql-${var.prefix}-${var.environment}")
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = coalesce(var.location, data.azurerm_resource_group.this.location)
+
+  sku_name                     = var.sku_name
+  zone                         = var.availability_zone
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
+
+  storage {
+    iops              = var.storage.iops
+    size_gb           = var.storage.size_gb
+    auto_grow_enabled = var.storage.auto_grow_enabled
+  }
+
+  administrator_login    = var.administrator_login != null ? var.administrator_login : "mysqladm"
+  administrator_password = var.administrator_password
+  version                = var.mysql_version
+ 
+  lifecycle {
+    ignore_changes = [
+      zone,
+    ]
+  }
+}
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "allowed_ips" {
+  count = length(var.allowed_ip_ranges)
+
+  name                = "allowed-ip-${count.index}"
+  resource_group_name = data.azurerm_resource_group.this.name
+  server_name         = azurerm_mysql_flexible_server.server.name
+  start_ip_address    = var.allowed_ip_ranges[count.index].from
+  end_ip_address      = var.allowed_ip_ranges[count.index].to
+  depends_on = [
+    azurerm_mysql_flexible_server.server
+  ]
+}
+
+resource "azurerm_mysql_flexible_server_configuration" "this" {
+  for_each = { for config in var.configurations : config["key"] => config }
+
+  name                = each.value.key
+  resource_group_name = azurerm_mysql_flexible_server.server.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.server.name
+  value               = each.value.value
+}
--- app/modules/azure/mysql/variables.tf
+++ app/modules/azure/mysql/variables.tf
@@ -1,1 +1,82 @@
 # This is where you put your variables declaration
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "administrator_login" {
+  type    = string
+  default = null
+}
+
+variable "administrator_password" {
+  type = string
+}
+
+variable "environment" {
+  type        = string
+  default     = "development"
+  description = "(Optional) Names the environment of the resource"
+}
+
+variable "sku_name" {
+  type    = string
+  default = "B_Gen5_1"
+}
+
+variable "storage" {
+  type        = object({
+    iops              = number
+    size_gb           = number
+    auto_grow_enabled = bool
+  })
+  default     = {
+    iops              = 360
+    size_gb           = 5
+    auto_grow_enabled = true
+  }
+  description = "(Optional) Defines storage options for DBMS"
+}
+
+variable "mysql_version" {
+  type    = string
+  default = "8.0"
+}
+
+variable "backup_retention_days" {
+  type    = number
+  default = 35
+}
+
+variable "geo_redundant_backup_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "allowed_ip_ranges" {
+  type = list(object({
+    from = string
+    to   = string
+  }))
+  default = []
+}
+
+variable "availability_zone" {
+  type    = number
+  default = 2
+}
+
+variable "configurations" {
+  type = list(object({
+    key   = string
+    value = string
+  }))
+  default = []
+}
