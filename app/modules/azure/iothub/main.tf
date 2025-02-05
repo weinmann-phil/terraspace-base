21f79d05b7239635999d50fd8938d27c911d8576
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-02-05 21:33:10 +0100

    feat(modules): add iothub (not functional)

--- app/modules/azure/iothub/main.tf
+++ app/modules/azure/iothub/main.tf
@@ -1,1 +1,86 @@
 # This is where you put your resource declaration
+locals {
+  tags = merge(var.tags, {
+    managed_by     = "OpenTofu"
+    module_name    = "azure/iothub"
+    module_version = "v0.0.1"
+    environment    = var.environment
+  })
+}
+
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_iothub" "this" {
+  name                = coalesce(var.iothub_name, "iothub-${var.prefix}-${var.environment}")
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = coalesce(var.location, data.azurerm_resource_group.this.location)
+  
+  local_authentication_enabled = var.enable_local_authentication
+
+  sku {
+    name     = var.sku.name
+    capacity = var.sku.capacity
+  }
+  
+  dynamic "endpoint" {
+    for_each = length(var.endoints) > 0 ? ["endpoints"] : []
+
+    contents = {
+      type                       = each.type
+      name                       = each.name
+      authentication_type        = each.authentication_type
+      identity_id                = each.identity_id
+      endpoint_uri               = each.uri
+      entity_path                = each.entity_path
+      connection_string          = each.connection_string
+      batch_frequency_in_seconds = each.batch_frequency_in_seconds
+      max_chunk_size_in_bytes    = each.max_chunk_size_in_bytes
+      container_name             = each.container_name
+      encoding                   = each.encoding
+      file_name_format           = each.file_name_format
+      resource_group_name        = each.resource_group_name
+    }
+  }
+
+  dynamic "route" {
+    for_each = length(var.routes) > 0 ? ["routes"] : []
+
+    contents = {
+      name           = each.name
+      source         = each.source
+      condition      = each.condition
+      endpoint_names = each.endpoint_name
+      enabled        = each.enabled
+    }
+  }
+
+  dynamic "enrichment" {
+    for_each = length(var.enrichments) > 0 ? ["enrichments"] : []
+
+    contents = {
+      key            = each.key
+      value          = each.value
+      endpoint_names = each.endpoint_names
+    }
+  }
+
+  dynamic "cloud_to_device" {
+    for_each = length(var.connections) > 0 ? ["connections"] : []
+
+    contents = {
+      max_delivery_count = each.max_delivery_count
+      default_ttl        = each.default_ttl
+      feedback {
+        time_to_live       = each.time_to_live
+        max_delivery_count = each.max_delivery_count
+        lock_duration      = each.lock_duration
+      }
+    }
+  }
+
+  tags = {
+    local.tags
+  }
+}
--- app/modules/azure/iothub/variables.tf
+++ app/modules/azure/iothub/variables.tf
@@ -1,1 +1,146 @@
 # This is where you put your variables declaration
+variable "enable_local_authentication" {
+  type        = bool
+  default     = false
+  description = "(Optional) Enables local authentication. Defaults to `false`."
+}
+
+variable "endpoints" {
+  type        = list(object({
+    type                       = string
+    name                       = string
+    authentication_type        = string
+    identity_id                = string
+    uri                        = string
+    entity_path                = string
+    connection_string          = string
+    batch_frequency_in_seconds = number
+    max_chunk_size_in_bytes    = number
+    container_name             = string
+    encoding                   = string
+    file_name_format           = string
+    resource_group_name        = string
+  }))
+  default     = []
+  description = "(Optional) Defines an endpoint for the IotHub."
+
+  validation {
+    condition     = ( 
+      alltrue([ for instance in var.endpoints : (  
+        contains(["AzureIotHub.StorageContainer", "AzureIotHub.ServiceBusQueue", "AzureIotHub.ServiceBusTopic", "AzureIotHub.EventHub"], instance.type))
+      )])
+    )
+    error_message = "The variable endpoint.type must be one of the following resource definitions: `AzureIotHub.StorageContainer`, `AzureIotHub.ServiceBusQueue`, `AzureIotHub.ServiceBusTopic`, or `AzureIotHub.EventHub`."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        !contains(["events", "operationsMonitoringEvents", "fileNotification", "$default"], instance.name)
+      )])
+    )
+    error_message = "Avoid using one of the following reserved names: `events`, `operationsMonitoringEvents`, `fileNotification`, or `$default`."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        contains(["keyBased", "identityBased"], instance.authentication_type))
+      )])
+    )
+    error_message = "The variable endpoint.authentication_type can either be `keyBased` or `identityBased`."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        (instance.type != "AzureIotHub.StorageContainer" && instance.authentication_type == "identityBased") && var.endpoint.endpoint_uri != null
+      )])
+    )
+    error_message = "The variable endpoint.uri cannot be null, if authentication_type is `identityBased` and endpoint type is not AzureIotHub.StorageContainer."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        (var.endpoint.type != "AzureIotHub.StorageContainer" && var.endpoint.authentication_type == "identityBased") && var.endpoint.endpoint_path != null
+      )])
+    )
+    error_message = "The variable endpoint.entity_path cannot be null, if authnetication_type is `identityBased` and endpoint type is not AzureIotHub.StorageContainer."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        var.endpoint.authentication_type == "keyBased" && var.endpoint.connection_string != null
+      )])
+    )
+    error_message = "The variable endpoint.connection_string cannot be null, if the authentication type is `keyBased`."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        var.endpoint.batch_frequency_in_seconds >= 60 && var.endpoint.batch_frequency_in_seconds <= 270
+      )])
+    )
+    error_message = "The frequency has to be a value between 60 and 270 seconds."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        var.endpoint.max_chunk_size_in_bytes >= 10485760 && var.endpoint.max_chunk_size_in_bytes <= 524288000
+      )])
+    )
+    error_message = "Please select a value between 10485760 (10MB) and 524288000 (500MB) bytes."
+  }
+
+  validation {
+    condition     = (
+      alltrue([ for instance in var.endpoints : (
+        contains(["Avro", "AvroDeflate", "JSON"], var.endpoint.encoding)
+      )])
+    )
+    error_message = "Valid values for endpoint.encoding are `Avro`, `AvroDeflate`, or `JSON`."
+  }
+}
+
+variable "environment" {
+  type        = string
+  default     = "development"
+  description = "(Optional) Defines the name of the resource environment. Defaults to `development`."
+}
+
+variable "iothub_name" {
+  type        = string
+  default     = null
+  description = "(Optional) Sets the name of Azure IotHub"
+}
+
+variable "location" {
+  type        = string
+  default     = null
+  description = "(Optional) Sets the location of the resource. Defaults to the location of the resource group."
+}
+
+variable "sku" {
+  type        = object({
+    name     = string
+    capacity = string
+  })
+  default     = {
+    name     = "F1"
+    capacity = "1"
+  }
+  description = "(Optional) Defines the SKU data of the iothub."
+
+  validation {
+    condition = (
+      contains(["F1", "B1", "B2", "B3", "S1", "S2", "S3"], var.sku.name)     
+    )
+    error_message = "SKU name must be one of the following values: `F1`, `B1`, `B2`, `B3`, `S1`, `S2`, `S3`."
+  }
+}
+
+
